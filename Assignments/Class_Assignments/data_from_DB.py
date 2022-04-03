1a72fdb824c3ec86b6e4897e7cd33f5c2f50e411
Roshan1704 <rimport17@gmail.com>
2022-04-04 01:11:13 +0530

    Get data from DB to APIs update

--- Assignments/Class_Assignments/data_from_DB.py
+++ Assignments/Class_Assignments/data_from_DB.py
@@ -20,15 +20,9 @@ def test_api_mongoDB():
     client = pymongo.MongoClient(
         "mongodb+srv://mongodb:mongodb@roshanmongodb.xeh33.mongodb.net/Ds_mongodb?retryWrites=true&w=majority")
     db = client.Ds_mongodb
-    all_data = {}
-    j = 0
     get = db.BC_Database
-    for i in get.find():
-        all_data[j] = i
-        j += 1
-    print(all_data)
-    return jsonify(all_data)
-
+    data = {"result": str(list(get.find()))}
+    return data
 
 if __name__ == '__main__':
     api_data_sql.run(debug=True)
